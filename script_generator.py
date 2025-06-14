7a51399e5114e100cb30b62b04b66ad5637aeda2
Chirag Gupta <43753048+chiraggupta8769@users.noreply.github.com>
2025-06-14 21:36:40 +0000

    Add script_generator module

---
+++ script_generator.py
@@ -0,0 +1,12 @@
+import random
+
+queries = [
+    "Power of believing in yourself",
+    "From failure to success",
+    "Chasing your dreams",
+    "Never give up",
+    "Turning pain into power"
+]
+
+def generate_script(query):
+    return f"{query} can change your life. Keep going, even when it’s tough."
